7177b3d53b50e1d0e6092b8b2adba8162699d36b
David Adeboye <david.adeboye@outlook.com>
2018-10-16 21:11:10 +0100

    Update travis

---
+++ .travis.sh
@@ -0,0 +1,22 @@
+set -e
+
+OPAM_DEPENDS="ocamlfind ounit re"
+OCAML_VERSION=4.07.0
+OPAM_VERSION=2.0.0
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+opam init 
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+make
+# make test
