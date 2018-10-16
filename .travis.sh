6173386fbb4296261ec55b04a059e7b93f31b0a8
David Adeboye <david.adeboye@outlook.com>
2018-10-16 23:28:39 +0100

    Organise files into subfolders

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,6 @@
 set -e
 
-OPAM_DEPENDS="ocamlfind ounit re"
-OCAML_VERSION=4.02.3
-OPAM_VERSION=1.2.2
+OPAM_DEPENDS="ocamlfind owl ounit re"
 
 echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
 sudo apt-get update -qq
--- src/main/Parliament.ml
+++ src/main/Parliament.ml
@@ -2,6 +2,5 @@
  * Parliament - A distributed general-purpose cluster-computing framework for OCaml
  * Copyright (c) 2018-2019 David Adeboye <doaa2@cl.cam.ac.uk>
  *)
-
-print_string "Hello world!\n";;
  
+module Worker = Parliament_worker
