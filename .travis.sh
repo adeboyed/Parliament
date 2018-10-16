2a073be8823884effdae90547fa9c07cd6648b79
David Adeboye <david.adeboye@outlook.com>
2018-10-16 21:31:24 +0100

    Change ppa

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ OPAM_DEPENDS="ocamlfind ounit re"
 OCAML_VERSION=4.02.3
 OPAM_VERSION=1.2.2
 
-echo "yes" | sudo add-apt-repository ppa:ocaml42+opam12
+echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
