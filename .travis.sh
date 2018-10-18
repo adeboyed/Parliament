fddd4c6da3fefffde660aed1b8edf48b4b603f33
David Adeboye <david.adeboye@outlook.com>
2018-10-18 09:56:31 +0100

    Remove owl dependency

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,10 @@
 set -e
 
-OPAM_DEPENDS="ocamlfind owl ounit"
+OPAM_DEPENDS="ocamlfind ounit"
 
 echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml-nox ocaml-native-compilers camlp4-extra opam libgsl-dev libshp-dev libplplot-dev
+sudo apt-get install -qq ocaml-nox ocaml-native-compilers camlp4-extra opam libgsl0-dev libshp-dev libplplot-dev
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
