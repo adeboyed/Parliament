be86f00e040af2d937fdea1270726013474e6206
David Adeboye <david.adeboye@outlook.com>
2018-12-12 12:56:22 +0000

    Fix initilisation problem

--- lib/house.ml
+++ lib/house.ml
@@ -26,7 +26,7 @@ let spec =
 
 let command =
   Command.basic
-    ~summary:"Parliament CLI"
+    ~summary:"Parliament - A distributed general-purpose cluster-computing framework for OCaml"
     spec
     (fun hn pt au () ->
        hostname := hn;
@@ -34,8 +34,9 @@ let command =
        auth := au;
     )
 
-let init_master = 
-  Command.run ~version:"1.0" ~build_info:"RWO" command;
+let init_master () =
+  Printf.printf "%s\n" (getenv "PARLIAMENT_MODE");
+  (* Command.run ~version:"1.0" ~build_info:"RWO" command; *)
   try (
     let ctx = connect !hostname !port !auth in
     let ic = open_in Sys.argv.(0) in
@@ -64,34 +65,43 @@ let validate_output map_type datapack =
   | (Single_in_variable_out, _) -> ()
   | _ -> raise IncorrectNumberOfOutputs
 
-let init_worker = 
-  let bytes = 
-    let len = in_channel_length stdin in 
-    let bytes = Bytes.create len in 
-    really_input stdin bytes 0 len; 
-    bytes 
-  in
-  let file_out = getenv "PARLIAMENT_OUTPUT" in
-  let worker_input = Parli_core_proto.Worker_pb.decode_worker_input(Pbrt.Decoder.of_bytes bytes) in
-  let datapack_in : datapack = Marshal.from_bytes worker_input.datapack 0 in 
-  let job_func : (datapack -> datapack) = Marshal.from_bytes worker_input.function_closure 0 in
-  let datapack_out = job_func datapack_in in
-  validate_output worker_input.map_type datapack_out;
-  let marshalled = Marshal.to_bytes datapack_out [Compat_32] in
-  let oc = open_out file_out in
-  output_bytes oc marshalled;
-  close_out oc;
-  exit 0
+let init_worker () = 
+  try (
+    let bytes_in = 
+      let len = in_channel_length stdin in 
+      let bytes = Bytes.create len in 
+      really_input stdin bytes 0 len; 
+      bytes 
+    in
+    let file_out = getenv "PARLIAMENT_OUTPUT" in
+    let worker_input = Parli_core_proto.Worker_pb.decode_worker_input(Pbrt.Decoder.of_bytes bytes_in) in
+    let datapack_in : datapack = Marshal.from_bytes worker_input.datapack 0 in 
+    let job_func : (datapack -> datapack) = Marshal.from_bytes worker_input.function_closure 0 in
+    let datapack_out = job_func datapack_in in
+    validate_output worker_input.map_type datapack_out;
+    let datapack_out_list = List.map (fun x -> Datapack.single x) (Array.to_list datapack_out.data) in
+    let worker_output = Parli_core_proto.Worker_types.({
+        datapacks = List.map (fun x -> Marshal.to_bytes x [Compat_32]) datapack_out_list
+      }) in
+    let encoder = Pbrt.Encoder.create () in
+    Parli_core_proto.Worker_pb.encode_worker_output worker_output encoder;
+    let oc = open_out file_out in
+    output_bytes oc (Pbrt.Encoder.to_bytes encoder);
+    close_out oc;
+    exit 0
+  )
+  with Not_found -> (Util.error_print("Please check you have initialised the correct ENV variables"); exit 3)
 
-let init =
+let init () =
   let internal_init =
     let running_option = getenv "PARLIAMENT_MODE" in
-    match running_option with
-    | "Worker" -> init_worker
-    | _ -> init_master
+    if Core.Std.String.contains running_option 'W' then 
+      init_worker()
+    else
+      init_master()
   in
   try internal_init
   with 
-    Not_found -> init_master
+    Not_found -> init_master()
   | NotConnnectedException -> (Util.error_print("Application was disconnected during context intialisation!"); exit 2)
 
